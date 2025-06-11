424ff5048fe778fc7a847ad0b854bcc45f45b5c1
v_guanglwen <v_guanglwen@tencent.com>
2025-06-11 17:14:56 +0800

    feat: 测试

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,7 +3,7 @@ const config = require('./config')
 const themeListeners = []
 global.isDemo = true
 App({
-console.log('App Launch111');
+console.log('App Launch11');
 
   onLaunch(opts, data) {
     const that = this
